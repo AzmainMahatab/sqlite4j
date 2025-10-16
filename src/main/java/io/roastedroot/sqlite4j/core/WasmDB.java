6265766690b92a9c7b65ebf5dbb02d899d521806
andreatp <andrea.peruffo1982@gmail.com>
2025-10-16 11:17:10 +0100

    Test, catch unsupported operation exception

--- src/main/java/io/roastedroot/sqlite4j/core/WasmDB.java
+++ src/main/java/io/roastedroot/sqlite4j/core/WasmDB.java
@@ -301,8 +301,12 @@ public class WasmDB extends DB implements WasmDBImports {
                         java.nio.file.Files.copy(is, dest, StandardCopyOption.REPLACE_EXISTING);
                         var owner = Files.getOwner(origin);
                         Files.setOwner(dest, owner);
-                        var permissions = Files.getPosixFilePermissions(origin);
-                        Files.setPosixFilePermissions(dest, permissions);
+                        try {
+                            var permissions = Files.getPosixFilePermissions(origin);
+                            Files.setPosixFilePermissions(dest, permissions);
+                        } catch (UnsupportedOperationException e) {
+                            // ignore
+                        }
                     } catch (IOException e) {
                         SQLException msg =
                                 DB.newSQLException(
